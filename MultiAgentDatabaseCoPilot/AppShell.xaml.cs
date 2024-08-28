6d50770cfd2da3981964879438558230d5e7e21f
Ali Reza Farahnak <alfarahn@microsoft.com>
2024-08-28 16:46:02 +0200

    m

---
+++ MultiAgentDatabaseCoPilot/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiAgentDatabaseCoPilot
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
